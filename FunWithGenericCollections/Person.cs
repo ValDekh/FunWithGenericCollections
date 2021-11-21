088c135c58c6de0b257bd3627623e5225d3ccce1
Валерий Дехтюк <dekhtiuk.v.s@gmail.com>
2021-11-22 00:26:05 +0200

    Добавьте файлы проекта.

---
+++ FunWithGenericCollections/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunWithGenericCollections
+{
+   public class Person
+    {
+        public int Age {get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person() { }
+        public Person(string firstName, string lastName, int age)
+        {
+            this.Age = age;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age} ";
+        }
+
+    }
+}
---
+++ FunWithGenericCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using FunWithGenericCollections;
+
+namespace FunWithGenericCollections
+{
+    class Program
+    {
+        static void UseGenericList()
+        {
+            List<Person> people = new List<Person>()
+                {
+                    new Person {FirstName = "Lala", LastName = "Ivanchuk", Age = 68 },
+                    new Person {FirstName = "Nikolai", LastName = "Ivanchuk", Age = 70 },
+                    new Person {FirstName = "Olgha", LastName = "Borisova", Age = 33},
+                    new Person {FirstName = "Boris", LastName = "Borisov", Age = 36}
+                };
+            Console.WriteLine($"Items in the list {people.Count}");
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            people.Insert(3, new Person { FirstName = "Jerar", LastName = "Viljan", Age = 35 });
+            Console.WriteLine($"Itens in the list {people.Count}");
+            Person[] arrayOfPeople = people.ToArray();
+            foreach (Person p in arrayOfPeople)
+            {
+                Console.WriteLine($"{p.FirstName} is their First Name");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            UseGenericList();
+        }
+    }
+}
